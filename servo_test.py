807608d40e5cb88383e3f6f354de2cea7ca362be
Andrej Cepygin <andrej.cepygin@uni-oldenburg.de>
2025-03-30 16:05:09 +0200

    testing around with pose detection

--- servo_test.py
+++ servo_test.py
@@ -1,11 +1,12 @@
-from gpiozero import Servo
+from gpiozero import AngularServo
 from gpiozero.pins.pigpio import PiGPIOFactory
+import os
 import time
 
-factory = PiGPIOFactory(host='192.168.178.74')
-servo = Servo(12)
+factory = PiGPIOFactory(host=os.environ["PIGPIO_ADDR"])
+servo = AngularServo(12)
 
 
-servo.max()
-time.sleep(1)
-servo.min()
+servo.max_angle = 90
+time.sleep(3)
+servo.min_angle = -90
