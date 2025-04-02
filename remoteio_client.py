8db688ac796ebd41bb6bc9729e81b48405d77cc1
Andrej Cepygin <andrej.cepygin@uni-oldenburg.de>
2025-04-02 13:19:20 +0200

    testing with remoteio

--- remoteio_client.py
+++ remoteio_client.py
@@ -1,4 +1,4 @@
-from remoteio import RemoteServer
+from remoteio import RemoteServer, Remote_AngularServo
 from time import sleep
 import os
 
@@ -8,5 +8,20 @@ if __name__ == "__main__":
     server_port = 8509
 
     rs = RemoteServer(server_ip, server_port)
+
+    rl=Remote_AngularServo(rs,21)
+    print(rl.all)
+    rl.max()
+    sleep(4)
+    rl.mid()
+    print(rl.pulse_width)
+    print(rl.max_pulse_width)
+    
+    sleep(4)
+    rl.min()
+    print(rl.is_active)
+    sleep(4)
+    rl.angle=rl.max_angle
+    sleep(5)
 
     
