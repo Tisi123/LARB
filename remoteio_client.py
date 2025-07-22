805f2e4ab7d145d41d82a7454ae73cc10385dacb
TisiCodes <104676640+Tisi123@users.noreply.github.com>
2025-07-22 13:31:59 +0200

    Update remoteio_client.py

--- remoteio_client.py
+++ remoteio_client.py
@@ -4,7 +4,7 @@ import os
 
 if __name__ == "__main__":
     #server_ip = os.environ["REMOTEIO_ADDR"]
-    server_ip = "192.168.178.74"
+    server_ip = ""
     server_port = 8509
 
     rs = RemoteServer(server_ip, server_port)
